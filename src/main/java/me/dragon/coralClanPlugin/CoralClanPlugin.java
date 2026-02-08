2958f02bee196b3cdaebe147352583791a12e43f
Dragon_nick <nicolalupo302@gmail.com>
2026-02-08 13:00:45 +0100

    Init

---
+++ src/main/java/me/dragon/coralClanPlugin/CoralClanPlugin.java
@@ -0,0 +1,17 @@
+package me.dragon.coralClanPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoralClanPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
